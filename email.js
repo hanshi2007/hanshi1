2a320847422a0a24b50a8f576e893b5f6908ced5
hanshi2007 <hanshikha4977@gmail.com>
2026-07-10 11:33:37 +0530

    created and added email.js file

---
+++ email.js
@@ -0,0 +1,25 @@
+const nodemailer = require('nodemailer');
+
+const sendEmail = async ({ to, subject, html, attachments }) => {
+  const transporter = nodemailer.createTransport({
+    host: process.env.EMAIL_HOST,
+    port: process.env.EMAIL_PORT,
+    secure: false, // true for 465, false for other ports
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASS,
+    },
+  });
+
+  const mailOptions = {
+    from: process.env.EMAIL_FROM,
+    to,
+    subject,
+    html,
+    attachments: attachments || [],
+  };
+
+  return transporter.sendMail(mailOptions);
+};
+
+module.exports = sendEmail;
